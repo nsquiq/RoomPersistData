b197d0d669f3365634b7c02a46def70cda83245e
Nuray <210103331@stu.sdu.edu.kz>
2023-12-05 15:41:17 +0600

    Codelab2:disable sell

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -43,7 +43,7 @@ class ItemDetailsViewModel(
         itemsRepository.getItemStream(itemId)
             .filterNotNull()
             .map {
-                ItemDetailsUiState(itemDetails = it.toItemDetails())
+                ItemDetailsUiState(outOfStock = it.quantity <= 0,itemDetails = it.toItemDetails())
             }.stateIn(
                 scope = viewModelScope,
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
